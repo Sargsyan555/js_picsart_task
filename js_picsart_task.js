e1a2bea667a124fec701d0b3df451ac1aafbdb1e
Picsart Academy <picsartacademy@Picsarts-MacBook-Pro-43.local>
2024-08-16 12:13:16 +0400

    first commit

---
+++ js_picsart_task.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to my temporary site</h1>
+    <h2>Romik Sargsayn</h2>
+    <img src="../../download.jpg" alt="">
+    <button onclick="alert('Hello, John Doe!')">Click Me</button>
+</body>
+</html>
